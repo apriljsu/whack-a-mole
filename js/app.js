493303a6777b0266953dd6768d7a68fd7a84cd8d
apriljsu <apriljsu@gmail.com>
2022-07-13 22:10:05 -0400

    added timer function and css for player buttons/timer&score display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log("hi")
+const secondEle=document.getElementById("second")
+let timer=60
+function countDown(){
+timer--
+if(timer<=0){
+  secondEle.innerText="00"
+  clearInterval(timeInterval)
+  alert("time is up")
+}else{
+  secondEle.innerText=timer
+}
+}
+// let timeInterval=setInterval(countDown,1000)
