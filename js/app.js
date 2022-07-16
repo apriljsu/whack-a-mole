07be208e34fb824ff09373ec9f7ac1a7565a37fd
apriljsu <apriljsu@gmail.com>
2022-07-16 09:29:09 -0700

    added arrow to indicate current player,in js, created variables to grab html elements and created function to select random hole

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 console.log("hi")
-const secondEle=document.getElementById("second")
-let timer=60
+//grab html elements
+const holes=document.querySelectorAll(".hole")
+const mole=document.querySelector(".mole")
+const second=document.querySelector("#second")
+const score=document.querySelector("#score")
+//game objects
+//declare global variables
+let timer=11
+//create random hole function for mole to appear
+function randomHole(){
+  //make sure no mole class was added to any hole
+  holes.forEach(
+    hole => {hole.classList.remove("mole")}
+  )
+  //select random hole and then add class Mole
+let randomHole=holes[Math.floor(Math.random()*9)]
+randomHole.classList.add("mole")
+}
+randomHole()
+//create timer function
 function countDown(){
 timer--
 if(timer<=0){
-  secondEle.innerText="00"
+  second.innerText="00"
   clearInterval(timeInterval)
   alert("time is up")
+}else
+if(timer>0 && timer<10){
+  second.innerText="0"+timer
 }else{
-  secondEle.innerText=timer
+  second.innerText=timer
 }
 }
 // let timeInterval=setInterval(countDown,1000)
